641a91a3472bed65eef50ce1f6f6c29f58bdc67b
parksol <thfdl0317@naver.com>
2024-05-16 12:56:41 +0900

    feat : firebase, emotion 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import * as PATH from "./utils/path";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import * as PATH from './utils/path';
+import { css } from '@emotion/react';
+import styled from 'styled-components';
+
+const containerStyles = css`
+  background-color: pink;
+`;
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
+`;
 
 function App() {
+  console.log(process.env);
+
   return (
-    <Routes>
-      <Route path={PATH.HOME} element={<Home />} />
-    </Routes>
+    <div>
+      <Button>test</Button>
+      <Routes>
+        <Route path={PATH.HOME} element={<Home />} />
+      </Routes>
+      <div css={containerStyles}>test</div>
+    </div>
   );
 }
 
