fcb174c79cb39841449ccc15e0abbf4da1df83bf
parksol <thfdl0317@naver.com>
2024-05-16 11:25:44 +0900

    test

--- src/components/common/Icon.tsx
+++ src/components/common/Icon.tsx
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import styled from 'styled-components';
+import React from 'react';
+import styled from 'styled-components';
 
 // export const ICON_TYPE = ICON_NAME;
 
-// export interface IconProps {
-//   // iconName: ICON_NAME_TYPE;
-//   size?: number;
-//   color?: string;
-//   onClick?: () => void;
-//   margin?: string;
-// }
+export interface IconProps {
+  // iconName: ICON_NAME_TYPE;
+  size?: number;
+  color?: string;
+  onClick?: () => void;
+  margin?: string;
+}
 
-// export const icon = ({ size = 24, color = 'blackSalon600' }: IconProps) => {
-//   return (
-//     <svg
-//       width={size}
-//       height={size}
-//       viewBox="0 0 24 24"
-//       fill="none"
-//       xmlns="http://www.w3.org/2000/svg"
-//     >
-//       {getIconSVGPath({ iconName, color })}
-//     </svg>
-//   );
-// };
+export const icon = ({ size = 24, color = 'blackSalon600' }: IconProps) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {/* {getIconSVGPath({ iconName, color })} */}
+    </svg>
+  );
+};
