7e6a833d251e6ba86796a2eecba3aadb352ac5af
parksol <thfdl0317@naver.com>
2024-05-16 14:07:04 +0900

    style : 공통 색상 추가, css 초기화 설정

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,39 @@
+import { css } from '@emotion/react';
+import { colorPalette } from './colorPalette';
+
+export default css`
+  ${colorPalette}
+
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-size: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  input:focus,
+  button:focus,
+  select:focus,
+  textarea:focus {
+    outline: none;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+  }
+`;
