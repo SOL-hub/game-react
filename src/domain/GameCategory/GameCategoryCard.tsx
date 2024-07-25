004928817b0c4546215099ed98bccc631a270af3
parksol <thfdl0317@naver.com>
2024-07-25 15:23:52 +0900

    feat: 대표카테고리 데이터 생성하여 적용

--- src/domain/GameCategory/GameCategoryCard.tsx
+++ src/domain/GameCategory/GameCategoryCard.tsx
@@ -4,7 +4,7 @@ import { Row } from 'components/common/Layout';
 import { HeadRegular18, BodyRegular15 } from 'components/common/Typo';
 import { IconButton, ICON_TYPE } from 'components/common/Icon';
 
-const GameCategoryCard = () => {
+const GameCategoryCard = ({ id, name, summary, detail }) => {
   return (
     <Card
       padding="2rem 1rem 2.5rem"
@@ -13,14 +13,12 @@ const GameCategoryCard = () => {
       borderRadius="0.75rem"
     >
       <Row justifyContent="space-between" alignItems="center" margin="0 0 1rem">
-        <HeadRegular18>전략 게임</HeadRegular18>
+        <HeadRegular18>{name}</HeadRegular18>
         <IconButton iconName={ICON_TYPE.arrowV1Down} color="redSalon500" />
       </Row>
-      <BodyRegular15 color="blackSalon500">머리를 써서 추리하거나 전략을 짭니다.</BodyRegular15>
+      <BodyRegular15 color="blackSalon500">{summary}</BodyRegular15>
       <BodyRegular15 color="blackSalon400" margin="1rem 0 0">
-        머리를 써서 추리하거나 전략을 짜서 상대방을 이기는 게임입니다. 비교적 규칙이 복잡하고,
-        상대방의 수를 읽고 생각을 필요로 합니다. 게임시간이 다른 종류의 게임에 비해 다소 긴 편이며,
-        난이도가 높습니다.
+        {detail}
       </BodyRegular15>
     </Card>
   );
